2a7bef471d1ddc4c435a0a5850278282bb73f212
karan123Sharma <karanjitendrasharma302@gmail.com>
2023-10-16 23:20:00 +0530

    commit

--- Backedn/index.js
+++ Backedn/index.js
@@ -1,16 +1,23 @@
-var express= require('express');
-var app=express(); 
+// var express= require('express');
+// var app=express(); 
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-    res.render('pages/index');
-});
+// app.get('/', function(req, res) {
+//     res.render('pages/index');
+// });
 
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
+// app.get('/about', function(req, res) {
+//   res.render('pages/about');
+// });
 
-app.listen(8080);
+// app.listen(8080);
 
-console.log('Server is listening on port 8080');
+// console.log('Server is listening on port 8080');
+var express =  require('express');
+var app = express();
+app.set('view engine','ejs');
+app.get('/',(req,res)=>{
+  res.render('pages/index');
+})
+app.listen(8000)
